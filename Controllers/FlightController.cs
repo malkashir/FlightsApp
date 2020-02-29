cc900cb719a0705d11b8d27506b1fa1105b1325e
ShirMal <51335669+malkashir@users.noreply.github.com>
2020-02-29 20:42:16 +0200

    Create FlightController.cs

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FlightsApp.Models;
+
+namespace FlightsApp.Controllers
+{
+    public class FlightController : ApiController
+    {
+        // GET api/<controller>
+        public List<Flight> Get()
+        {
+            //Flight flight = new Flight();
+            return Flight.FlightList;
+            return Flight.FlightList;
+
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]Flight flight)
+        {
+            Flight.FlightList.Add(flight); //add to ist
+            //return Flight.flightList;
+        }
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
