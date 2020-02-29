8f91d47772742280b9c77e001d4b0b216a2a023d
ShirMal <51335669+malkashir@users.noreply.github.com>
2020-02-29 20:58:04 +0200

    Add files via upload

--- Controllers/FlightController.cs
+++ Controllers/FlightController.cs
@@ -13,22 +13,14 @@ namespace FlightsApp.Controllers
         // GET api/<controller>
         public List<Flight> Get()
         {
-            //Flight flight = new Flight();
-            return Flight.FlightList;
             return Flight.FlightList;
 
         }
 
-        ////public List<Flight> Get(int id)
-        ////{
-        ////    return Flight.FlightList;
-        ////}
-
         // POST api/<controller>
         public void Post([FromBody]Flight flight)
         {
-            Flight.FlightList.Add(flight); //add to ist
-            //return Flight.flightList;
+            Flight.FlightList.Add(flight); 
         }
         // PUT api/<controller>/5
         public void Put(int id, [FromBody]string value)
